e4742636f9db43a1d7c1f01ecb93b4a6740072ee
차민규 <chac_2@naver.com>
2025-04-16 17:44:29 +0900

    Initial commit

---
+++ online_lab/django_ws_2_1/main.py
@@ -0,0 +1,32 @@
+import requests
+
+API_URL = 'http://www.aladin.co.kr/ttb/api/ItemList.aspx'
+API_KEY = 'ttbchacmanz1920001'
+params = {
+    'ttbkey': API_KEY,
+    'QueryType' : 'ItemNewSpecial',
+    'MaxResults' : 50,
+    'SearchTarget' : 'Book',
+    'Output' : 'JS',
+    'Version' : 20131101,
+}
+
+response = requests.get(API_URL,params=params).json()
+# print(response['item'])
+
+# ctrl + shift + `
+
+result = []
+for item in response['item']:
+    info = {
+        '제목' : item['title'],
+        '저자' : item['author'],
+        '출간일' : item['pubDate'],
+        '국제 표준 도서 번호' : item['isbn'],
+    }
+    #print(info)
+    result.append(info)
+
+
+from pprint import pprint
+pprint(result)
